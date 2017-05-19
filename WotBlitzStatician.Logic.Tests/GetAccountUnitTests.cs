7c216dff74c21c7ad2bb194e54482c8611c0f8b6
DrMboga <dr.mboga@yahoo.com>
2017-05-19 16:04:52 +0300

    Account Unit tests extended

--- WotBlitzStatician.Logic.Tests/GetAccountUnitTests.cs
+++ WotBlitzStatician.Logic.Tests/GetAccountUnitTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
@@ -20,7 +20,7 @@ namespace WotBlitzStatician.Logic.Tests
 
 		private readonly Random _random = new Random();
 
-		private IBlitzStaticianLogic _blitzStaticianLogicMock;
+		private IBlitzStaticianLogic _blitzStaticianLogic;
 		private Mock<IBlitzStaticianDataAccessor> _blitzStaticianDataAccessorMock;
 		private Mock<IWargamingApiClient> _wgApiClientMock;
 		private AccountInfo _expectedAccount;
@@ -36,7 +36,7 @@ namespace WotBlitzStatician.Logic.Tests
 
             _blitzStaticianDataAccessorMock = new Mock<IBlitzStaticianDataAccessor>();
             _wgApiClientMock = new Mock<IWargamingApiClient>();
-            _blitzStaticianLogicMock = new BlitzStaticianLogic(_blitzStaticianDataAccessorMock.Object, _wgApiClientMock.Object);
+            _blitzStaticianLogic = new BlitzStaticianLogic(_blitzStaticianDataAccessorMock.Object, _wgApiClientMock.Object);
 
 			_expectedAccount = new AccountInfo
 			{
@@ -52,7 +52,7 @@ namespace WotBlitzStatician.Logic.Tests
             _blitzStaticianDataAccessorMock.Setup(accessor => accessor.GetAccountInfo(It.IsAny<string>()))
                                            .Returns(_expectedAccount);
 
-			var account = await _blitzStaticianLogicMock.GetAccount(string.Empty);
+			var account = await _blitzStaticianLogic.GetAccount(string.Empty);
 
             Assert.NotNull(account); //, "Existing account wasn't found");
             Assert.Equal(_expectedAccount.NickName, account.NickName); //, "Account nicknames doesn't match");
@@ -69,8 +69,49 @@ namespace WotBlitzStatician.Logic.Tests
             _blitzStaticianDataAccessorMock
                 .Verify(accessor => accessor.SaveTanksStatistic(It.IsAny<List<AccountTankStatistics>>()), Times.Never());
 
-            _log.Debug("GetAccountUnitTests.GetExistingAccountFromDb passed");
+            _log.Debug("GetExistingAccountFromDb passed");
         }
 
+
+	    [Fact]
+		public async Task GetExistingAccountFirstTime()
+		{
+			_blitzStaticianDataAccessorMock.Setup(accessor => accessor.GetAccountInfo(It.IsAny<string>()))
+				.Returns<AccountInfo>(null);
+			_wgApiClientMock.Setup(client => client.FindAccount(It.IsAny<string>()))
+				.Returns(Task.FromResult(_expectedAccount));
+
+			var account = await _blitzStaticianLogic.GetAccount(string.Empty);
+
+			Assert.NotNull(account); //, "Existing account wasn't found");
+			Assert.Equal(_expectedAccount.NickName, account.NickName); //, "Account nicknames doesn't match");
+			Assert.Equal(_expectedAccount.AccountId, account.AccountId); //, "Account ids doesn't match");
+
+			_blitzStaticianDataAccessorMock
+				.Verify(accessor => accessor.GetAccountInfo(It.IsAny<string>()), Times.Once());
+			_wgApiClientMock
+				.Verify(apiClient => apiClient.FindAccount(It.IsAny<string>()), Times.Once());
+			_wgApiClientMock
+				.Verify(apiClient => apiClient.GetTanksStatisticks(It.IsAny<long>()), Times.Once());
+			_blitzStaticianDataAccessorMock
+				.Verify(accessor => accessor.SaveAccountInfo(It.IsAny<AccountInfo>()), Times.Once());
+			_blitzStaticianDataAccessorMock
+				.Verify(accessor => accessor.SaveTanksStatistic(It.IsAny<List<AccountTankStatistics>>()), Times.Once());
+
+			_log.Debug("GetExistingAccountFirstTime passed");
+		}
+
+	    [Fact]
+		public async Task GetExistingAccountFirstTimeNotFound()
+		{
+			_blitzStaticianDataAccessorMock.Setup(accessor => accessor.GetAccountInfo(It.IsAny<string>()))
+				.Returns<AccountInfo>(null);
+			_wgApiClientMock.Setup(client => client.FindAccount(It.IsAny<string>()))
+				.Returns(Task.FromResult<AccountInfo>(null));
+
+			var ex = await Assert.ThrowsAsync<ArgumentException>(() => _blitzStaticianLogic.GetAccount(string.Empty));
+
+			_log.Debug($"GetExistingAccountFirstTimeNotFound passed. Exception message: '{ex.Message}'");
+		}
 	}
 }
