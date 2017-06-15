244e0ee6e738050ac70b6d859c8996ed5b005ec5
DrMboga <dr.mboga@yahoo.com>
2017-06-15 13:31:40 +0300

    WG Api client construction

--- WotBlitzStatician.WotApiClient/WargamingApiClient.cs
+++ WotBlitzStatician.WotApiClient/WargamingApiClient.cs
@@ -1,13 +1,14 @@
 ﻿namespace WotBlitzStatician.WotApiClient
 {
 	using System.Collections.Generic;
+	using System.Linq;
 	using System.Threading.Tasks;
+	using WotBlitzStatician.WotApiClient.InternalModel;
+	using WotBlitzStatician.WotApiClient.Mappers;
 	using WotBlitzStaticitian.Model;
 
 	public class WargamingApiClient : IWargamingApiClient
 	{
-		// ToDo: Get AutoMapper, create internal ctructures and mappers
-
 		private readonly IWgApiConfiguration _configuration;
 
 		public WargamingApiClient(IWgApiConfiguration configuration)
@@ -15,32 +16,56 @@
 			_configuration = configuration;
 		}
 
-		public Task<List<VehicleEncyclopedia>> GetWontEncyclopediaVehicles()
+		public async Task<List<VehicleEncyclopedia>> GetWotEncyclopediaVehiclesAsync()
 		{
-//			var webClient = new WebApiClient();
-//
-//			var tankopedia = webClient.GetResponse<Dictionary<string, WotEncyclopediaVehiclesResponse>>(
-//				_configuration.BaseAddress,
-//				string.Format(_configuration.VehiclesEncyclopediaRequestAddressTemplate, _configuration.ApplicationId)
-//			).Result;
-//			return tankopedia.Values.ToList();
+			var webClient = new WebApiClient();
 
-			throw new System.NotImplementedException();
+			var tankopedia = await webClient.GetResponse<Dictionary<string, WotEncyclopediaVehiclesResponse>>(
+				_configuration.BaseAddress,
+				string.Format(_configuration.VehiclesEncyclopediaRequestAddressTemplate, _configuration.ApplicationId));
+			var allVehicles = tankopedia.Values.ToList();
+			allVehicles.AddMarkI();
+			allVehicles.AddHetzerKame();
+
+			var mapper = new TankopediaMapper();
+			return allVehicles.Select(t => mapper.Map(t)).ToList();
 		}
 
-		public Task<AccountInfo> FindAccount(string nickName)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		public Task<AccountInfo> GetAccountInfoAllStatistics(long accountId)
+		public async Task<AccountInfo> FindAccountAsync(string nickName)
 		{
 			throw new System.NotImplementedException();
 		}
 
-		public Task<List<AccountTankStatistics>> GetTanksStatisticks(long accountId)
+		public async Task<AccountInfo> GetAccountInfoAllStatisticsAsync(string accountId)
 		{
-			throw new System.NotImplementedException();
+			var webClient = new WebApiClient();
+
+			var accountInfo = await webClient.GetResponse<Dictionary<string, WotAccountInfoResponse>>(
+					_configuration.BaseAddress,
+					string.Format(_configuration.AccountStatRequestAddressTemplate, _configuration.ApplicationId, accountId));
+
+			var accountInfoResponse = accountInfo[accountId];
+			var accountInfoMapper = new AccountInfoMapper();
+
+			var accountInfoMapped = accountInfoMapper.Map(accountInfoResponse);
+
+			var accountInfoStatMapper = new AccountInfoStatisticsMapper();
+			accountInfoMapped.AccountInfoStatistics = accountInfoStatMapper.Map(accountInfoResponse);
+			// ToDo: Map PrivateInfo
+
+			return accountInfoMapped;
+		}
+
+		public async Task<List<AccountTankStatistics>> GetTanksStatisticsAsync(string accountId)
+		{
+			var webClient = new WebApiClient();
+
+			var tanksStat = await webClient.GetResponse<Dictionary<string, List<WotAccountTanksStatResponse>>>(
+				_configuration.BaseAddress,
+				string.Format(_configuration.AccountTanksStatisticRequestAddressTemplate, _configuration.ApplicationId, accountId));
+
+			var mapper = new TanksStatMapper();
+			return tanksStat[accountId].Select(s => mapper.Map(s)).ToList();
 		}
 	}
 }
