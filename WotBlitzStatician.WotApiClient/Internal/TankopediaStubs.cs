2776f2f4d491011b5d00c48e626f2784a598d42f
DrMboga <dr.mboga@yahoo.com>
2017-06-15 13:33:58 +0300

    WG Api client construction

---
+++ WotBlitzStatician.WotApiClient/Internal/TankopediaStubs.cs
@@ -0,0 +1,49 @@
+﻿namespace WotBlitzStatician.WotApiClient
+{
+	using System.Collections.Generic;
+	using System.Linq;
+	using WotBlitzStatician.WotApiClient.InternalModel;
+
+	internal static class TankopediaStubs
+	{
+		/// <summary>
+		/// It's a cratch. There is no MarkI tank in the API.
+		/// </summary>
+		/// <param name="vehicles"></param>
+		public static void AddMarkI(this List<WotEncyclopediaVehiclesResponse> vehicles)
+		{
+			if (vehicles.Any(v => v.TankId == 64081))
+				return;
+			vehicles.Add(new WotEncyclopediaVehiclesResponse
+				{
+					TankId = 64081,
+					Name = "Mk I* HeavyTank",
+					Tier = 1,
+					Nation = "uk",
+					Type = "heavyTank",
+					IsPremium = true
+				}
+			);
+		}
+
+		/// <summary>
+		/// It's a cratch. There is no Hetzer Kame Sp yet tank in the API.
+		/// </summary>
+		/// <param name="vehicles"></param>
+		public static void AddHetzerKame(this List<WotEncyclopediaVehiclesResponse> vehicles)
+		{
+			if (vehicles.Any(v => v.TankId == 52065))
+				return;
+			vehicles.Add(new WotEncyclopediaVehiclesResponse
+				{
+					TankId = 52065,
+					Name = "Hetzer Kame SP",
+					Tier = 4,
+					Nation = "japan",
+					Type = "AT-SPG",
+					IsPremium = true
+				}
+			);
+		}
+	}
+}
---
+++ WotBlitzStatician.WotApiClient/Internal/TimeExtrensions.cs
@@ -0,0 +1,25 @@
+﻿namespace WotBlitzStatician.WotApiClient
+{
+	using System;
+
+	internal  static class TimeExtrensions
+	{
+		public static DateTime? ToDateTime(this int? unixTimeStamp)
+		{
+			if (!unixTimeStamp.HasValue)
+				return null;
+			// Unix timestamp is seconds past epoch
+			var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+			dtDateTime = dtDateTime.AddSeconds(unixTimeStamp.Value).ToLocalTime();
+			return dtDateTime;
+		}
+
+		public static TimeSpan? ToTimeSpan(this int? unixTimeStamp)
+		{
+			if (!unixTimeStamp.HasValue)
+				return null;
+			return TimeSpan.FromSeconds(unixTimeStamp.Value);
+		}
+
+	}
+}
