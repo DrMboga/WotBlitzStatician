92d59cfc64555c07a182b5fd224ae9cb8a450c10
DrMboga <dr.mboga@yahoo.com>
2017-06-16 11:57:06 +0300

    WotApi client finished and tested

---
+++ WotBlitzStatician.WotApiClient/InternalModel/WotAccountListResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace WotBlitzStatician.WotApiClient.InternalModel
+{
+	using Newtonsoft.Json;
+
+	internal class WotAccountListResponse
+	{
+		///<summary>
+		///Идентификатор аккаунта игрока
+		///</summary>
+		[JsonProperty("account_id")]
+		public long? AccountId { get; set; }
+
+		///<summary>
+		///Имя игрока
+		///</summary>
+		[JsonProperty("nickname")]
+		public string Nickname { get; set; }
+	}
+}
