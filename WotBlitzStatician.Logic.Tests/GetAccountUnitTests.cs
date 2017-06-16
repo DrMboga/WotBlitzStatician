595abb060c025c0edb4bf8c5cf50ea870e6491b5
DrMboga <dr.mboga@yahoo.com>
2017-06-16 11:48:56 +0300

    WotApi client finished and tested

--- WotBlitzStatician.Logic.Tests/GetAccountUnitTests.cs
+++ WotBlitzStatician.Logic.Tests/GetAccountUnitTests.cs
@@ -59,7 +59,7 @@ namespace WotBlitzStatician.Logic.Tests
 			_wgApiClientMock
 				.Verify(apiClient => apiClient.FindAccountAsync(It.IsAny<string>()), Times.Never());
 			_wgApiClientMock
-				.Verify(apiClient => apiClient.GetTanksStatisticsAsync(It.IsAny<string>()), Times.Never());
+				.Verify(apiClient => apiClient.GetTanksStatisticsAsync(It.IsAny<long>()), Times.Never());
 			_blitzStaticianDataAccessorMock
 				.Verify(accessor => accessor.SaveAccountInfo(It.IsAny<AccountInfo>()), Times.Never());
 			_blitzStaticianDataAccessorMock
@@ -75,9 +75,11 @@ namespace WotBlitzStatician.Logic.Tests
 			_blitzStaticianDataAccessorMock.Setup(accessor => accessor.GetAccountInfo(It.IsAny<string>()))
 				.Returns<AccountInfo>(null);
 			_wgApiClientMock.Setup(client => client.FindAccountAsync(It.IsAny<string>()))
+				.Returns(Task.FromResult(new List<AccountInfo>{_expectedAccount}));
+			_wgApiClientMock.Setup(client => client.GetAccountInfoAllStatisticsAsync(It.IsAny<long>()))
 				.Returns(Task.FromResult(_expectedAccount));
 
-			var account = await _blitzStaticianLogic.GetAccount(string.Empty);
+			var account = await _blitzStaticianLogic.GetAccount(_expectedAccount.NickName);
 
 			Assert.NotNull(account); //, "Existing account wasn't found");
 			Assert.Equal(_expectedAccount.NickName, account.NickName); //, "Account nicknames doesn't match");
@@ -88,7 +90,9 @@ namespace WotBlitzStatician.Logic.Tests
 			_wgApiClientMock
 				.Verify(apiClient => apiClient.FindAccountAsync(It.IsAny<string>()), Times.Once());
 			_wgApiClientMock
-				.Verify(apiClient => apiClient.GetTanksStatisticsAsync(It.IsAny<string>()), Times.Once());
+				.Verify(apiClient => apiClient.GetAccountInfoAllStatisticsAsync(It.IsAny<long>()), Times.Once());
+			_wgApiClientMock
+				.Verify(apiClient => apiClient.GetTanksStatisticsAsync(It.IsAny<long>()), Times.Once());
 			_blitzStaticianDataAccessorMock
 				.Verify(accessor => accessor.SaveAccountInfo(It.IsAny<AccountInfo>()), Times.Once());
 			_blitzStaticianDataAccessorMock
@@ -103,7 +107,7 @@ namespace WotBlitzStatician.Logic.Tests
 			_blitzStaticianDataAccessorMock.Setup(accessor => accessor.GetAccountInfo(It.IsAny<string>()))
 				.Returns<AccountInfo>(null);
 			_wgApiClientMock.Setup(client => client.FindAccountAsync(It.IsAny<string>()))
-				.Returns(Task.FromResult<AccountInfo>(null));
+				.Returns(Task.FromResult(new List<AccountInfo> {null}));
 
 			var ex = await Assert.ThrowsAsync<ArgumentException>(() => _blitzStaticianLogic.GetAccount(string.Empty));
 
