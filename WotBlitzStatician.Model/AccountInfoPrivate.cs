4b594b3a48adc87a55ba3cc0b4d7e76d031f62fc
DrMboga <dr.mboga@yahoo.com>
2017-06-21 12:05:27 +0300

    Model expansion and fix namespace

--- WotBlitzStatician.Model/AccountInfoPrivate.cs
+++ WotBlitzStatician.Model/AccountInfoPrivate.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace WotBlitzStaticitian.Model
+﻿namespace WotBlitzStatician.Model
 {
+	using System;
+	using System.ComponentModel.DataAnnotations;
+	using System.ComponentModel.DataAnnotations.Schema;
+	
 	public class AccountInfoPrivate
 	{
 		[Key]
