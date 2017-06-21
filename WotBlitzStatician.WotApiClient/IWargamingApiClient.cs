44f333319a64c4b026d4c764f7d7cd8e01e1f991
DrMboga <dr.mboga@yahoo.com>
2017-06-21 12:09:31 +0300

    Model expansion and fix namespace

--- WotBlitzStatician.WotApiClient/IWargamingApiClient.cs
+++ WotBlitzStatician.WotApiClient/IWargamingApiClient.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using WotBlitzStaticitian.Model;
-
-namespace WotBlitzStatician.WotApiClient
+﻿namespace WotBlitzStatician.WotApiClient
 {
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+	using WotBlitzStatician.Model;
+
 	public interface IWargamingApiClient
 	{
 		Task<List<VehicleEncyclopedia>> GetWotEncyclopediaVehiclesAsync();
