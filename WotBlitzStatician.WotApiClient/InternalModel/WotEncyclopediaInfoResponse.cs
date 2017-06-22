10269b3e064c4b4a6079bbd327209c69514c7bd5
DrMboga <dr.mboga@yahoo.com>
2017-06-22 16:05:13 +0200

    New Wot Api requests

---
+++ WotBlitzStatician.WotApiClient/InternalModel/WotEncyclopediaInfoResponse.cs
@@ -0,0 +1,67 @@
+﻿namespace WotBlitzStatician.WotApiClient.InternalModel
+{
+	using System.Collections.Generic;
+	using Newtonsoft.Json;
+
+	internal class WotEncyclopediaInfoResponse
+	{
+		///<summary>
+		///Версия игрового клиента
+		///</summary>
+		[JsonProperty("game_version")]
+		public string GameVersion { get; set; }
+
+		///<summary>
+		///Cписок поддерживаемых языков
+		///</summary>
+		[JsonProperty("languages")]
+		public Dictionary<string, string> Languages { get; set; }
+
+		///<summary>
+		///Время обновления информации о технике в энциклопедии
+		///</summary>
+		[JsonProperty("tanks_updated_at")]
+		public int? TanksUpdatedAt { get; set; }
+
+		///<summary>
+		///Возможные специальности экипажа
+		///</summary>
+		[JsonProperty("vehicle_crew_roles")]
+		public Dictionary<string, string> VehicleCrewRoles { get; set; }
+
+		///<summary>
+		///Доступные нации
+		///</summary>
+		[JsonProperty("vehicle_nations")]
+		public Dictionary<string, string> VehicleNations { get; set; }
+
+		///<summary>
+		///Возможные типы техники
+		///</summary>
+		[JsonProperty("vehicle_types")]
+		public Dictionary<string, string> VehicleTypes { get; set; }
+
+		///<summary>
+		///Разделы наград
+		///</summary>
+		[JsonProperty("achievement_sections")]
+		public WotEncyclopediaInfoAchievement_sections AchievementSections { get; set; }
+	}
+
+	internal class WotEncyclopediaInfoAchievement_sections
+	{
+
+		///<summary>
+		///Название раздела наград
+		///</summary>
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		///<summary>
+		///Порядок раздела наград
+		///</summary>
+		[JsonProperty("order")]
+		public long? Order { get; set; }
+
+	}
+}
