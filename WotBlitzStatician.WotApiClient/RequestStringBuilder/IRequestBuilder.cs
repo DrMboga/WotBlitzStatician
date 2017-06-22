6dbeb19fabcbd4816155dab8866c0062597089cc
DrMboga <dr.mboga@yahoo.com>
2017-06-22 15:59:19 +0200

    DI container introduction

---
+++ WotBlitzStatician.WotApiClient/RequestStringBuilder/IRequestBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace WotBlitzStatician.WotApiClient.RequestStringBuilder
+{
+	internal interface IRequestBuilder
+	{
+		string BaseAddress { get; }
+		string BuildRequestUrl(RequestType requestType, params RequestParameter[] parameters);
+	}
+}
