ef9b2e31bdb5a551d4828b9231da26872bf9e0f5
DrMboga <dr.mboga@yahoo.com>
2017-06-22 15:57:00 +0200

    DI container introduction

---
+++ WotBlitzStatician.WotApiClient/WargamingApiClientInstaller.cs
@@ -0,0 +1,20 @@
+﻿namespace WotBlitzStatician.WotApiClient
+{
+	using Autofac;
+	using WotBlitzStatician.WotApiClient.RequestStringBuilder;
+
+	public static class WargamingApiClientInstaller
+	{
+		public static void ConfigureWargamingApi(this ContainerBuilder containerBuilder)
+		{
+			// ToDo: register the parameters factory and inject it into RequestBuilder
+
+			containerBuilder.RegisterType<RequestBuilder>().As<IRequestBuilder>();
+			// Manually calling the internal constructor
+			containerBuilder.Register(c => new WargamingApiClient(c.Resolve<IRequestBuilder>()))
+				.As<IWargamingApiClient>();
+
+		}
+
+	}
+}
