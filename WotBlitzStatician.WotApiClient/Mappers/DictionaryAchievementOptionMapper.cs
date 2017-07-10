169043933b5466217c2ea15abdcee7c8973b10ff
DrMboga <dr.mboga@yahoo.com>
2017-07-10 16:36:24 +0300

    Achievements dictionary added

---
+++ WotBlitzStatician.WotApiClient/Mappers/DictionaryAchievementOptionMapper.cs
@@ -0,0 +1,29 @@
+﻿namespace WotBlitzStatician.WotApiClient.Mappers
+{
+	using AutoMapper;
+	using WotBlitzStatician.Model;
+	using WotBlitzStatician.WotApiClient.InternalModel;
+
+	internal class DictionaryAchievementOptionMapper : IMapper<WotEncyclopediaAchievementsOptions, AchievementOption>
+	{
+		public DictionaryAchievementOptionMapper()
+		{
+			Mapper.Initialize(m => m.CreateMap<WotEncyclopediaAchievementsOptions, AchievementOption>()
+				.ForMember(dest => dest.Description, op => op.MapFrom(s => s.Description))
+				.ForMember(dest => dest.Image, op => op.MapFrom(s => s.Image))
+				.ForMember(dest => dest.ImageBig, op => op.MapFrom(s => s.ImageBig))
+				.ForMember(dest => dest.Name, op => op.MapFrom(s => s.Name))
+			);
+		}
+
+		public AchievementOption Map(WotEncyclopediaAchievementsOptions source)
+		{
+			return Mapper.Map<WotEncyclopediaAchievementsOptions, AchievementOption>(source);
+		}
+
+		public AchievementOption Map(WotEncyclopediaAchievementsOptions source, AchievementOption destination)
+		{
+			return Mapper.Map(source, destination);
+		}
+	}
+}
---
+++ WotBlitzStatician.WotApiClient/Mappers/DictionaryAchievementsMapper.cs
@@ -0,0 +1,33 @@
+﻿namespace WotBlitzStatician.WotApiClient.Mappers
+{
+	using AutoMapper;
+	using WotBlitzStatician.Model;
+	using WotBlitzStatician.WotApiClient.InternalModel;
+
+	internal class DictionaryAchievementsMapper : IMapper<WotEncyclopediaAchievementsResponse, Achievement>
+	{
+		public DictionaryAchievementsMapper()
+		{
+			Mapper.Initialize(m => m.CreateMap<WotEncyclopediaAchievementsResponse, Achievement>()
+				.ForMember(dest => dest.AchievementId, op => op.MapFrom(s => s.AchievementId))
+				.ForMember(dest => dest.Condition, op => op.MapFrom(s => s.Condition))
+				.ForMember(dest => dest.Description, op => op.MapFrom(s => s.Description))
+				.ForMember(dest => dest.Image, op => op.MapFrom(s => s.Image))
+				.ForMember(dest => dest.ImageBig, op => op.MapFrom(s => s.ImageBig))
+				.ForMember(dest => dest.Name, op => op.MapFrom(s => s.Name))
+				.ForMember(dest => dest.Order, op => op.MapFrom(s => s.Order))
+				.ForMember(dest => dest.Section, op => op.MapFrom(s => s.Section))
+			);
+		}
+
+		public Achievement Map(WotEncyclopediaAchievementsResponse source)
+		{
+			return Mapper.Map<WotEncyclopediaAchievementsResponse, Achievement>(source);
+		}
+
+		public Achievement Map(WotEncyclopediaAchievementsResponse source, Achievement destination)
+		{
+			return Mapper.Map(source, destination);
+		}
+	}
+}
