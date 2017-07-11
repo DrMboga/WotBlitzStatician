084c8d5c0ef272a878b68068466e1c1e28a89d5e
DrMboga <dr.mboga@yahoo.com>
2017-07-11 16:54:26 +0300

    AchievementsInfo added

---
+++ WotBlitzStatician.WotApiClient/InternalModel/WotAccountAchievementResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace WotBlitzStatician.WotApiClient.InternalModel
+{
+	using System.Collections.Generic;
+	using Newtonsoft.Json;
+
+	internal class WotAccountAchievementResponse
+	{
+		///<summary>
+		///Полученные достижения
+		///</summary>
+		[JsonProperty("achievements")]
+		public Dictionary<string, string> Achievements { get; set; }
+
+		///<summary>
+		///Максимальные значения серийных достижений
+		///</summary>
+		[JsonProperty("max_series")]
+		public Dictionary<string, string> MaxSeries { get; set; }
+	}
+}
---
+++ WotBlitzStatician.WotApiClient/InternalModel/WotAccountTankAchievementResponse.cs
@@ -0,0 +1,32 @@
+﻿namespace WotBlitzStatician.WotApiClient.InternalModel
+{
+	using System.Collections.Generic;
+	using Newtonsoft.Json;
+
+	internal class WotAccountTankAchievementResponse
+	{
+		///<summary>
+		///Идентификатор аккаунта игрока
+		///</summary>
+		[JsonProperty("account_id")]
+		public long? AccountId { get; set; }
+
+		///<summary>
+		///Полученные достижения
+		///</summary>
+		[JsonProperty("achievements")]
+		public Dictionary<string, string> Achievements { get; set; }
+
+		///<summary>
+		///Максимальные значения серийных достижений
+		///</summary>
+		[JsonProperty("max_series")]
+		public Dictionary<string, string> MaxSeries { get; set; }
+
+		///<summary>
+		///Идентификатор техники
+		///</summary>
+		[JsonProperty("tank_id")]
+		public long? TankId { get; set; }
+	}
+}
