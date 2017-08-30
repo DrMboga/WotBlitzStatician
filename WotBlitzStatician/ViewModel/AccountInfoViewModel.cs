9fb667e8be4cca2fff90c845f722d4a00f8a0afb
DrMboga <dr.mboga@yahoo.com>
2017-08-30 16:21:25 +0300

    Account info page just created

---
+++ WotBlitzStatician/ViewModel/AccountInfoViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WotBlitzStatician.ViewModel
+{
+	public class AccountInfoViewModel
+	{
+		public long AccountId { get; set; }
+	}
+}
---
+++ WotBlitzStatician/ViewModel/SearchPlayerViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WotBlitzStatician.ViewModel
+{
+	using System.Collections.Generic;
+	using WotBlitzStatician.Model;
+
+	public class SearchPlayerViewModel
+	{
+		public string SearchingNick { get; set; }
+
+		public List<AccountInfo> Accounts { get; set; }
+
+	}
+}
