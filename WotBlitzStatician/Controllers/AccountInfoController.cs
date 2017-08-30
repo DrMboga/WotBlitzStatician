7eef268197f4ac2640fc55e2bc1bb7cd8b3851d2
DrMboga <dr.mboga@yahoo.com>
2017-08-30 16:20:08 +0300

    Account info page just created

---
+++ WotBlitzStatician/Controllers/AccountInfoController.cs
@@ -0,0 +1,16 @@
+﻿namespace WotBlitzStatician.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+	using WotBlitzStatician.ViewModel;
+
+	public class AccountInfoController : Controller
+    {
+
+		public IActionResult Details([FromRoute(Name = "id")]long accountId)
+		{
+			var viewModel = new AccountInfoViewModel {AccountId = accountId};
+			return View(viewModel);
+		}
+
+	}
+}
