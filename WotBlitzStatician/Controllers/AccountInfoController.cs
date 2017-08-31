af63fcece6f6d1de02b7247d54e28293ec4bb759
DrMboga <dr.mboga@yahoo.com>
2017-08-31 12:03:30 +0300

    New accounts logic

--- WotBlitzStatician/Controllers/AccountInfoController.cs
+++ WotBlitzStatician/Controllers/AccountInfoController.cs
@@ -1,14 +1,26 @@
 ﻿namespace WotBlitzStatician.Controllers
 {
+	using System.Threading.Tasks;
 	using Microsoft.AspNetCore.Mvc;
+	using WotBlitzStatician.Logic;
 	using WotBlitzStatician.ViewModel;
 
 	public class AccountInfoController : Controller
     {
+	    private readonly IBlitzStaticianLogic _blitzStaticianLogic;
 
-		public IActionResult Details([FromRoute(Name = "id")]long accountId)
-		{
-			var viewModel = new AccountInfoViewModel {AccountId = accountId};
+	    public AccountInfoController(IBlitzStaticianLogic blitzStaticianLogic)
+	    {
+		    _blitzStaticianLogic = blitzStaticianLogic;
+	    }
+
+	    public async Task<IActionResult> Details([FromRoute(Name = "id")]long accountId)
+	    {
+		    var viewModel = new AccountInfoViewModel();
+			viewModel.AccountInfo = await _blitzStaticianLogic.GetAccountStatistics(accountId);
+
+			_blitzStaticianLogic.SetLastLoggedAccount(accountId);
+
 			return View(viewModel);
 		}
 
