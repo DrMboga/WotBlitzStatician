b8f655355e0d2153658adfdc6ec7c4e7454c4824
DrMboga <dr.mboga@yahoo.com>
2017-09-06 17:05:27 +0300

    Js for color gradations

--- WotBlitzStatician/Controllers/AccountInfoController.cs
+++ WotBlitzStatician/Controllers/AccountInfoController.cs
@@ -4,7 +4,6 @@
 	using Microsoft.AspNetCore.Mvc;
 	using WotBlitzStatician.Logic;
     using WotBlitzStatician.Mappers;
-    using WotBlitzStatician.ViewModel;
 
 	public class AccountInfoController : Controller
     {
@@ -23,11 +22,6 @@
             var viewModel = accountInfoMapper.Map(accountInfo);
 
 			_blitzStaticianLogic.SetLastLoggedAccount(accountId);
-
-			// toDo: Create mapper
-		    viewModel.Wn7Style = viewModel.Wn7Grade.GetWn7GradationStyle();
-		    viewModel.Wn7GradeS = viewModel.Wn7Grade.ToString();
-
 
 			return View(viewModel);
 		}
