f188d5cd25c15ca5d9f944425bbba8bf8829c930
DrMboga <dr.mboga@yahoo.com>
2017-09-07 11:12:19 +0300

    Clan info added to the view

--- WotBlitzStatician/Mappers/AccountInfoMapper.cs
+++ WotBlitzStatician/Mappers/AccountInfoMapper.cs
@@ -10,23 +10,33 @@
 
         public AccountInfoMapper()
         {
-	        _mapper = new Mapper(new MapperConfiguration(m => m.CreateMap<AccountInfo, AccountInfoViewModel>()
-		        .ForMember(dest => dest.AccountId, op => op.MapFrom(s => s.AccountId))
-		        .ForMember(dest => dest.NickName, op => op.MapFrom(s => s.NickName))
-		        .ForMember(dest => dest.LastBattleTime, op => op.MapFrom(s => s.LastBattleTime))
-		        .ForMember(dest => dest.Battles, op => op.MapFrom(s => s.AccountInfoStatistics.Battles))
-		        .ForMember(dest => dest.Wins, op => op.MapFrom(s => s.AccountInfoStatistics.Wins))
-		        .ForMember(dest => dest.Winrate, op => op
-			        .MapFrom(s => (decimal) s.AccountInfoStatistics.Wins * 100 / s.AccountInfoStatistics.Battles))
-		        .ForMember(dest => dest.WinrateGrade, op => op.MapFrom(s => ((decimal)s.AccountInfoStatistics.Wins * 100 / s.AccountInfoStatistics.Battles).GetWinrateGrade()))
-				.ForMember(dest => dest.Wn7, op => op.MapFrom(s => s.AccountInfoStatistics.Wn7))
-		        .ForMember(dest => dest.Wn7Grade, op => op.MapFrom(s => s.AccountInfoStatistics.Wn7.GetWn7Grade()))
-		        .ForMember(dest => dest.AvgDamage, op => op
-			        .MapFrom(s => s.AccountInfoStatistics.DamageDealt / s.AccountInfoStatistics.Battles))
-		        .ForMember(dest => dest.AvgXp, op => op
-			        .MapFrom(s => s.AccountInfoStatistics.Xp / s.AccountInfoStatistics.Battles))
-		        .ForMember(dest => dest.AvgTier, op => op.MapFrom(s => s.AccountInfoStatistics.AvgTier))
-	        ));
+	        _mapper = new Mapper(new MapperConfiguration(
+				m =>
+				{
+					m.CreateMap<AccountInfo, AccountInfoViewModel>()
+				        .ForMember(dest => dest.AccountId, op => op.MapFrom(s => s.AccountId))
+				        .ForMember(dest => dest.NickName, op => op.MapFrom(s => s.NickName))
+				        .ForMember(dest => dest.LastBattleTime, op => op.MapFrom(s => s.LastBattleTime))
+				        .ForMember(dest => dest.Battles, op => op.MapFrom(s => s.AccountInfoStatistics.Battles))
+				        .ForMember(dest => dest.Wins, op => op.MapFrom(s => s.AccountInfoStatistics.Wins))
+				        .ForMember(dest => dest.Winrate, op => op
+					        .MapFrom(s => (decimal) s.AccountInfoStatistics.Wins * 100 / s.AccountInfoStatistics.Battles))
+				        .ForMember(dest => dest.WinrateGrade,
+					        op => op.MapFrom(s =>
+						        ((decimal) s.AccountInfoStatistics.Wins * 100 / s.AccountInfoStatistics.Battles).GetWinrateGrade()))
+				        .ForMember(dest => dest.Wn7, op => op.MapFrom(s => s.AccountInfoStatistics.Wn7))
+				        .ForMember(dest => dest.Wn7Grade, op => op.MapFrom(s => s.AccountInfoStatistics.Wn7.GetWn7Grade()))
+				        .ForMember(dest => dest.AvgDamage, op => op
+					        .MapFrom(s => s.AccountInfoStatistics.DamageDealt / s.AccountInfoStatistics.Battles))
+				        .ForMember(dest => dest.AvgXp, op => op
+					        .MapFrom(s => s.AccountInfoStatistics.Xp / s.AccountInfoStatistics.Battles))
+				        .ForMember(dest => dest.AvgTier, op => op.MapFrom(s => s.AccountInfoStatistics.AvgTier));
+					m.CreateMap<AccountClanInfo, AccountClanInfoViewModel>()
+						.ForMember(dest => dest.ClanId, op => op.MapFrom(s => s.ClanId))
+						.ForMember(dest => dest.ClanTag, op => op.MapFrom(s => s.ClanTag))
+						.ForMember(dest => dest.ClanName, op => op.MapFrom(s => s.ClanName))
+						.ForMember(dest => dest.JoinedAt, op => op.MapFrom(s => s.PlayerJoinedAt));
+				}));
 
 
         }
