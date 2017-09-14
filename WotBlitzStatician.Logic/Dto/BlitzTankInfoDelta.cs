a44717ab636fa93dd237e6b3632689a080fc6804
DrMboga <dr.mboga@yahoo.com>
2017-09-14 14:05:08 +0300

    Tanks viewComponent

---
+++ WotBlitzStatician.Logic/Dto/BlitzTankInfoDelta.cs
@@ -0,0 +1,29 @@
+﻿namespace WotBlitzStatician.Logic.Dto
+{
+	using System;
+	using WotBlitzStatician.Model;
+
+	public class BlitzTankInfoDelta
+	{
+		public long AccountId { get; set; }
+		public long TankId { get; set; }
+		public string Name { get; set; }
+		public long Tier { get; set; }
+		public string Nation { get; set; }
+		public string Type { get; set; }
+		public bool IsPremium { get; set; }
+		public string PreviewImageUrl { get; set; }
+		public string NormalImageUrl { get; set; }
+		public MarkOfMastery MarkOfMastery { get; set; }
+		public string MarkOfMasteryImageUrl { get; set; }
+
+		public ValueDelta<long, long> Battles { get; set; }
+		public ValueDelta<DateTime, TimeSpan> LastBattle { get; set; }
+		public ValueDelta<long, long> Wins { get; set; }
+		public ValueDelta<decimal, decimal> Winrate { get; set; }
+		public ValueDelta<decimal, decimal> AvgDamage { get; set; }
+		public ValueDelta<decimal, decimal> AvgXp { get; set; }
+		public ValueDelta<decimal, decimal> Wn7 { get; set; }
+		public ValueDelta<decimal, decimal> Effectivity { get; set; }
+	}
+}
