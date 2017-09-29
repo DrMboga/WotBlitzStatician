be6cd3c28efba4eca187447377e9451a495ce005
DrMboga <dr.mboga@yahoo.com>
2017-09-29 16:48:14 +0300

    Mapper factory refactoring

---
+++ WotBlitzStatician.Model/MapperLogic/IMapperHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace WotBlitzStatician.Model.MapperLogic
+{
+	public interface IMapperHelper
+	{
+		TDestination Map<TSource, TDestination>(TSource source);
+		TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
+	}
+}
