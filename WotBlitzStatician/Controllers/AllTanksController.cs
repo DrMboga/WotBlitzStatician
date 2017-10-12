a90bd9afcda53b123876d39a657ec2368853210b
DrMboga <dr.mboga@yahoo.com>
2017-10-12 14:41:36 +0200

    Tank stat refactoring

---
+++ WotBlitzStatician/Controllers/AllTanksController.cs
@@ -0,0 +1,23 @@
+﻿namespace WotBlitzStatician.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using WotBlitzStatician.Logic;
+    using WotBlitzStatician.ViewModel;
+
+    public class AllTanksController : Controller
+    {
+        private readonly IBlitzStatitianDataAnalyser _dataAnalyser;
+
+        public AllTanksController(IBlitzStatitianDataAnalyser dataAnalyser)
+        {
+            _dataAnalyser = dataAnalyser;
+        }
+
+        public async Task<IActionResult> Details(long accountId)
+        {
+            var viewModel = new AllTanksViewModel {AccountId = accountId};
+            return View(viewModel);
+        }
+    }
+}
