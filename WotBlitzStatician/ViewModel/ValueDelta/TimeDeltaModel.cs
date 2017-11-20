602a34c2c42adf37a982a2a9ab4e3bf177b6b24d
DrMboga <dr.mboga@yahoo.com>
2017-11-20 15:46:53 +0300

    Added BatteLifeTime info

---
+++ WotBlitzStatician/ViewModel/ValueDelta/TimeDeltaModel.cs
@@ -0,0 +1,16 @@
+﻿namespace WotBlitzStatician.ViewModel.ValueDelta
+{
+	using System;
+	using System.ComponentModel.DataAnnotations;
+
+	public class TimeDeltaModel
+	{
+		public TimeSpan PresentValue { get; set; }
+		public TimeSpan PastValue { get; set; }
+		[DisplayFormat(DataFormatString = "+{0}")]
+		public TimeSpan Delta { get; set; }
+		public TimeSpan Interval { get; set; }
+		public bool IsNegative { get; set; }
+
+	}
+}
