eaccfe8de57ab04d600fd4ee0c97b384f26817b7
Michael <shabanov.mikhail@gmail.com>
2018-01-28 11:52:16 +0300

    WepApi added

---
+++ WotBlitzStatician/Controllers/DictionariesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WotBlitzStatician.WotApiClient;
+
+namespace WotBlitzStatician.Controllers
+{
+	[Route("api/[controller]")]
+    public class DictionariesController : Controller
+    {
+		private readonly IWargamingApiClient _wgApi;
+
+		public DictionariesController(IWargamingApiClient wgApi)
+		{
+			_wgApi = wgApi;
+		}
+
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+			// GetStaticDictionariesAsync
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
