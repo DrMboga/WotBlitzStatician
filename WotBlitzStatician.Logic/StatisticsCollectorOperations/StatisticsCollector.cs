7cc19cdd63c76f88a077578578de2a1572c83961
DrMboga <dr.mboga@yahoo.com>
2018-02-16 17:22:58 +0300

    Statistics collector operations factory

--- WotBlitzStatician.Logic/StatisticsCollectorOperations/StatisticsCollector.cs
+++ WotBlitzStatician.Logic/StatisticsCollectorOperations/StatisticsCollector.cs
@@ -2,12 +2,42 @@
 
 namespace WotBlitzStatician.Logic.StatisticsCollectorOperations
 {
+	using Autofac;
+	using WotBlitzStatician.Logic.StatisticsCollectorOperations.Operations;
+
 	public class StatisticsCollector : IStatisticsCollector
 	{
-		// ToDo: BeginLifeTymeScope
+		private readonly ILifetimeScope _childScope;
+		private readonly int _operationsCount;
+
+		public StatisticsCollector(ILifetimeScope lifetimeScope)
+		{
+			(_childScope, _operationsCount) = CreateOperationSteps(lifetimeScope);
+		}
 
 		public async Task CollectAllStatistics()
 		{
+			var operationContext = new StatisticsCollectorOperationContext();
+			for (int i = 0; i < _operationsCount; i++)
+			{
+				var operation = _childScope.ResolveKeyed<IStatisticsCollectorOperation>(i);
+				await operation.Execute(operationContext);
+			}
+		}
+
+		private static (ILifetimeScope, int) CreateOperationSteps(ILifetimeScope lifetimeScope)
+		{
+			var childScope = lifetimeScope.BeginLifetimeScope(builder =>
+			{
+				builder.RegisterType<ReadAccountInfosToCollect>()
+					.Keyed<IStatisticsCollectorOperation>(0);
+			});
+
+			// ToDo: Increase this number if adding new operation to scope.
+			// It's an autofac restriction - We can't iterate through IIndex<IStatisticsCollectorOperation, int>
+			int operationsCount = 1;
+
+			return (childScope, operationsCount);
 		}
 
 		#region IDisposable Support
@@ -19,7 +49,7 @@ namespace WotBlitzStatician.Logic.StatisticsCollectorOperations
 			{
 				if (disposing)
 				{
-					// TODO: Dispose LifeTymeScope
+					_childScope.Dispose();
 				}
 				disposedValue = true;
 			}
