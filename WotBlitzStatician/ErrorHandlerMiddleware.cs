89b86318c7a9012b7b1545bf8d307e66702d2e8f
Michael <shabanov.mikhail@gmail.com>
2018-04-07 20:58:04 +0300

    Error handling middleware

---
+++ WotBlitzStatician/ErrorHandlerMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace WotBlitzStatician
+{
+	public class ErrorHandlerMiddleware
+    {
+		private readonly RequestDelegate _next;
+		private readonly ILogger<ErrorHandlerMiddleware> _logger;
+
+		public ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger)
+		{
+			_next = next;
+			_logger = logger;
+		}
+
+		public async Task InvokeAsync(HttpContext context)
+		{
+			try
+			{
+				await _next(context);
+			}
+			catch (Exception e)
+			{
+				await HandleExceptionAsync(context, e, _logger);
+			}
+		}
+
+		private static Task HandleExceptionAsync(HttpContext context, Exception exception, ILogger logger)
+		{
+			var code = HttpStatusCode.InternalServerError; // 500 if unexpected
+
+			logger.LogError(default(EventId), exception, $"{context.Request.Method}: {context.Request.Path}");
+
+			var result = JsonConvert.SerializeObject(new { error = exception.Message });
+			context.Response.ContentType = "application/json";
+			context.Response.StatusCode = (int)code;
+			return context.Response.WriteAsync(result);
+		}
+
+	}
+
+	public static class ErrorHandlerMiddlewareExtensions
+	{
+		public static IApplicationBuilder UseErrorHandler(
+			this IApplicationBuilder builder)
+		{
+			return builder.UseMiddleware<ErrorHandlerMiddleware>();
+		}
+	}
+}
--- WotBlitzStatician/Startup.cs
+++ WotBlitzStatician/Startup.cs
@@ -45,10 +45,8 @@ namespace WotBlitzStatician
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+
+			app.UseErrorHandler();
 
 			app.UseMvc();
 		}
