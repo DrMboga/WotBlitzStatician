21efa16e00d20367d241d9c559dfa242a3729585
DrMboga <dr.mboga@yahoo.com>
2018-07-23 17:50:02 +0300

    Moving to achievements component

--- WotBlitzStatician/client-src/app/components/account-achievements/account-achievements.component.ts
+++ WotBlitzStatician/client-src/app/components/account-achievements/account-achievements.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-account-achievements',
+  selector: 'account-achievements',
   templateUrl: './account-achievements.component.html',
   styleUrls: ['./account-achievements.component.css']
 })
 export class AccountAchievementsComponent implements OnInit {
 
+  @Input("achievements")
+  public achievements: any[];
+
+  public battleAchievements: any[];
+  public epicAchievements: any[];
+  public platoonAchievements: any[];
+
+
   constructor() { }
 
   ngOnInit() {
+    this.battleAchievements = this.achievements.filter(
+      achievement => achievement.section === 'battle');
+    this.epicAchievements = this.achievements.filter(
+      achievement => achievement.section === 'epic');
+    this.platoonAchievements = this.achievements.filter(
+      achievement => achievement.section === 'platoon');
+
   }
 
 }
