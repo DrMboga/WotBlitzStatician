601349102cd80b2aec4bfc5ec9a99de0c5ffdd07
Dr_Mboga <dr.mboga@yahoo.com>
2018-10-24 22:12:55 +0300

    Font awesome icons

--- WotBlitzStatician/client-src/app/components/icons.module.ts
+++ WotBlitzStatician/client-src/app/components/icons.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faClock, } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faGamepad, faFlag, faSkullCrossbones } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faStar, faHeart } from '@fortawesome/free-regular-svg-icons';
  
 // Add an icon to the library for convenient access in other components
-library.add(faUser, faClock);
+library.add(faUser, faClock, faGamepad, faFlag, faSkullCrossbones, faStar, faHeart);
 
 @NgModule({
   imports: [
