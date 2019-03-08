070c1e32dd900489df1dfc5a445fa507fe41a93f
Michael Shabanov <shabanov.mikhail@gmail.com>
2019-03-08 09:09:25 +0300

    import database

--- WotBlitzStatician.Data/DataAccessors/Impl/ReplicationDataAccessor.cs
+++ WotBlitzStatician.Data/DataAccessors/Impl/ReplicationDataAccessor.cs
@@ -1,15 +1,21 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using WotBlitzStatician.Model;
 
 namespace WotBlitzStatician.Data.DataAccessors.Impl
 {
   public class ReplicationDataAccessor : IReplicationDataAccessor
   {
+    private ILogger<ReplicationDataAccessor> _logger;
     private readonly BlitzStaticianDbContext _dbContext;
 
-    public ReplicationDataAccessor(BlitzStaticianDbContext dbContext)
+    public ReplicationDataAccessor(BlitzStaticianDbContext dbContext,
+        ILogger<ReplicationDataAccessor> logger)
     {
+      _logger = logger;
       _dbContext = dbContext;
     }
 
@@ -36,7 +42,67 @@ namespace WotBlitzStatician.Data.DataAccessors.Impl
 
     public void SetDatabase(ReplicationData replicationData)
     {
-      throw new System.NotImplementedException();
+      if (_dbContext.AccountInfo.AsNoTracking().Count() != 0)
+      {
+        throw new ApplicationException("Database is not empty");
+      }
+
+      var exPresentAccountTanks = new List<AccountTankStatistics>();
+
+      replicationData.AccountInfoStatistics.ForEach(s => s.AccountInfoStatisticsId = 0);
+      replicationData.AccountClanHistory.ForEach(h => h.AccountClanHistoryId = 0);
+      replicationData.AccountClanInfo.ForEach(c => c.AccountClanInfoId = 0);
+      replicationData.AccountInfoAcievements.ForEach(c => c.AccountInfoAchievementId = 0);
+      replicationData.Frags.ForEach(f => f.FragListItemId = 0);
+      replicationData.PresentAccountTanks.ForEach(t =>
+      {
+        t.PresentAccountTankId = 0;
+        var stat = replicationData.AccountTanksStatistics.First(s => s.AccountTankStatisticId == t.AccountTankStatisticId);
+        exPresentAccountTanks.Add(new AccountTankStatistics
+        {
+          AccountTankStatisticId = stat.AccountTankStatisticId,
+          AccountId = stat.AccountId,
+          TankId = stat.TankId,
+          BattleLifeTimeInSeconds = stat.BattleLifeTimeInSeconds
+        });
+      });
+      replicationData.AccountTanksStatistics.ForEach(s => s.AccountTankStatisticId = 0);
+
+
+
+      try
+      {
+        _dbContext.Database.BeginTransaction();
+        _dbContext.AccountInfo.AddRange(replicationData.AccountInfo);
+        _dbContext.AccountInfoStatistics.AddRange(replicationData.AccountInfoStatistics);
+        _dbContext.AccountClanHistory.AddRange(replicationData.AccountClanHistory);
+        _dbContext.AccountClanInfo.AddRange(replicationData.AccountClanInfo);
+        _dbContext.AccountInfoAchievement.AddRange(replicationData.AccountInfoAcievements);
+        _dbContext.Frags.AddRange(replicationData.Frags);
+        _dbContext.AccountTankStatistics.AddRange(replicationData.AccountTanksStatistics);
+
+        _dbContext.SaveChanges();
+
+        foreach (var presentTank in replicationData.PresentAccountTanks)
+        {
+          var exAccountTankStat = exPresentAccountTanks
+                .First(t => t.AccountTankStatisticId == presentTank.AccountTankStatisticId);
+          presentTank.AccountTankStatisticId = _dbContext.AccountTankStatistics
+                .Where(t => t.AccountId == exAccountTankStat.AccountId && t.TankId == exAccountTankStat.TankId && t.BattleLifeTimeInSeconds == exAccountTankStat.BattleLifeTimeInSeconds)
+                .Select(t => t.AccountTankStatisticId)
+                .First();
+          _dbContext.PresentAccountTanks.Add(presentTank);
+        }
+
+        _dbContext.SaveChanges();
+
+        _dbContext.Database.CommitTransaction();
+      }
+      catch (Exception e)
+      {
+        _logger.LogError(e, "Cannot update database");
+        _dbContext.Database.RollbackTransaction();
+      }
     }
   }
 }
